52a37e77a1f5b160a5ddfe27fa3fbc859062320e
MikeChang <q0126354987@gmail.com>
2022-03-03 19:32:43 +0800

    try 2

--- JP_TRADE_SYSTEM/settings.py
+++ JP_TRADE_SYSTEM/settings.py
@@ -80,25 +80,25 @@ WSGI_APPLICATION = 'JP_TRADE_SYSTEM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "mssql",
-        "NAME": "ARTHGUI_齊民",
-        "USER": "guisa",
-        "PASSWORD": "gu!7669588",
-        "HOST": "210.67.182.196\sql2008r2",
-        "PORT": "1689",
-        "OPTIONS": {
-            "driver": "ODBC Driver 17 for SQL Server",
-        },
-    },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "mssql",
+#         "NAME": "ARTHGUI_齊民",
+#         "USER": "guisa",
+#         "PASSWORD": "gu!7669588",
+#         "HOST": "210.67.182.196\sql2008r2",
+#         "PORT": "1689",
+#         "OPTIONS": {
+#             "driver": "ODBC Driver 17 for SQL Server",
+#         },
+#     },
+# }
 
 
 # Password validation
